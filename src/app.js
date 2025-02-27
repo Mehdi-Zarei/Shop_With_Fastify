3aaabd81195c86ca638ca27921016b8a3a55b016
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-02-27 20:52:21 +0330

    refactor app initialization; update to use fastify, add route prefixing, and register error handlers

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
-const app = require("fastify")();
+const fastify = require("fastify")();
 
-module.exports = app;
+//* Import Files
+
+const {
+  NotFoundErrorHandler,
+  GlobalErrorHandler,
+} = require("../helpers/ErrorHandlers");
+
+//* Packages
+
+//* Hooks
+fastify.addHook("onRoute", (routeOptions) => {
+  routeOptions.url = "/api/v1" + routeOptions.url;
+});
+
+//* Import Routes
+const productRoutes = require("./product/V1/product.routes");
+
+//* Register Routes
+fastify.register(productRoutes, { prefix: "/products" });
+
+//* 404  & General Error Handler
+fastify.register(NotFoundErrorHandler);
+fastify.register(GlobalErrorHandler);
+
+module.exports = fastify;
