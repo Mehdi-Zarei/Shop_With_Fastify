ed98622e77c003a3d15b8971284682984589c130
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-02-27 16:56:36 +0330

    add User model; define schema with fields for id, name, phone, email, password, role, and isRestrict

---
+++ models/User.js
@@ -0,0 +1,49 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../configs/db");
+
+const user = sequelize.define(
+  "user",
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM,
+      defaultValue: "USER",
+      values: ["ADMIN", "USER"],
+      allowNull: false,
+    },
+    isRestrict: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  },
+  {
+    paranoid: true,
+    freezeTableName: true,
+    // timestamps: true, //todo:check it
+  }
+);
+
+module.exports = user;
