edd112e75c5c0b7f654c8fa839f8632f2c28d448
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-02-27 16:56:12 +0330

    refactor Redis connection handling; add connectRedis function for improved error management

--- configs/redis.js
+++ configs/redis.js
@@ -2,13 +2,20 @@ const { default: Redis } = require("ioredis");
 
 const redisClient = new Redis(process.env.REDIS_URI);
 
-redisClient.on("connect", () => {
-  console.log("✅ Connect To Redis Successfully.");
-});
+const connectRedis = async () => {
+  try {
+    await redisClient.ping();
+    console.log("✅ Connected to Redis Successfully.");
+  } catch (error) {
+    console.error("❌ Redis connection error:", error);
+    redisClient.quit();
+    process.exit(1);
+  }
+};
 
 redisClient.on("error", (error) => {
-  console.error("❌ Redis connection error:", error);
+  console.error("❌ Redis error:", error);
   redisClient.quit();
 });
 
-module.exports = redisClient;
+module.exports = { redisClient, connectRedis };
