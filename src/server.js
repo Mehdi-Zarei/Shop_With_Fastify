98b6b567e3124f8c83b9fe36ee3b20b040200c24
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-02-27 16:56:40 +0330

    refactor server initialization; connect to database and Redis before starting the server

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const app = require("./app");
 const port = process.env.PORT;
-const db = require("../configs/db");
-const Redis = require("../configs/redis");
+const { connectDB } = require("../configs/db");
+const { syncDB } = require("../models/index");
+const { connectRedis } = require("../configs/redis");
 
 const startServer = async () => {
   try {
+    await connectDB();
+    await syncDB();
+    await connectRedis();
+
     app.listen({ port });
     console.log(`🚀 Server is up and running at: http://localhost:${port}`);
   } catch (error) {
