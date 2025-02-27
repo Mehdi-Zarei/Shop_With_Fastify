bb75272c9ac77dae9289f7fb0ff8e487ca45f266
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-02-27 16:56:25 +0330

    add Product model; define schema with fields for id, name, slug, price, stock, description, subCategory, filterValues, images, and shortIdentifier

---
+++ models/Product.js
@@ -0,0 +1,58 @@
+const { DataTypes } = require("sequelize");
+const { sequelize } = require("../configs/db");
+
+const product = sequelize.define(
+  "product",
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    slug: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    price: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    subCategory: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      allowNull: false,
+    },
+    filterValues: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    images: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    shortIdentifier: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+  },
+  {
+    paranoid: true,
+    freezeTableName: true,
+    timestamps: true,
+  }
+);
+
+module.exports = product;
