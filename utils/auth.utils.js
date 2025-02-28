48b08017a931adb2d4572f5c1c8a30643384c080
Mehdi zarei <mahdizareiofficial@gmail.com>
2025-02-28 17:36:22 +0330

    add auth utility functions; implement password hashing and JWT token generation

---
+++ utils/auth.utils.js
@@ -0,0 +1,38 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+
+const hashPassword = async (password, salt) => {
+  return bcrypt.hashSync(password, salt);
+};
+
+const verifyPassword = async (password, hashedPassword) => {
+  return bcrypt.compareSync(password, hashedPassword);
+};
+
+const hashRefreshToken = async (refreshToken, salt) => {
+  return bcrypt.hashSync(refreshToken, salt);
+};
+
+const generateAccessToken = async (userID, userRole) => {
+  return jwt.sign(
+    { id: userID, role: userRole },
+    process.env.ACCESS_TOKEN_SECRET_KEY,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRE_TIME,
+    }
+  );
+};
+
+const generateRefreshToken = async (userID) => {
+  return jwt.sign({ id: userID }, process.env.REFRESH_TOKEN_SECRET_KEY, {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRE_TIME,
+  });
+};
+
+module.exports = {
+  hashPassword,
+  verifyPassword,
+  hashRefreshToken,
+  generateAccessToken,
+  generateRefreshToken,
+};
